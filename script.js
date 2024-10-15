df40a72a707911a5c54f18844f5ef917684134e0
Murilo Coelho <m.coelhos1212@gmail.com>
2024-10-14 23:05:37 -0300

    feat: create new form to address and search for possible ceps from form

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const form = document.querySelector('.form')
+const cepForm = document.querySelector('.cep-form')
+const addressForm = document.querySelector('.address-form')
 
-form.addEventListener("submit", async (event) => {
+cepForm.addEventListener("submit", async (event) => {
   event.preventDefault()
 
   searchCep()
+})
+
+addressForm.addEventListener("submit", async (event) => {
+  event.preventDefault()
+
+  searchAddress()
 })
 
 function searchCep() {
@@ -22,11 +29,38 @@ function searchCep() {
   })
 }
 
+function searchAddress() {
+  const address = {
+    logradouro: document.getElementById('logradouro').value,
+    cidade: document.getElementById('cidade').value,
+    uf: document.getElementById('uf').value,
+  }
+
+  if (!address.logradouro || !address.cidade || !address.uf) {
+    alert('Preencha todos os campos');
+    return;
+  }
+
+  const cidade = address.cidade.replace(' ', '%20');
+  const logradouro = address.logradouro.replace(' ', '+');
+
+  const url = `https://viacep.com.br/ws/${address.uf}/${cidade}/${logradouro}/json/`;
+
+  console.log(url)
+
+  fetch(url, {
+    method: 'GET',
+  }).then(response => response.json()).then(data => {
+    // renderData(data)
+    console.log(data)
+  })
+}
+
 function renderData(data) {
   const { logradouro, bairro, localidade, estado } = data;
 
-  document.getElementById('logradouro').innerHTML = logradouro;
-  document.getElementById('bairro').innerHTML = bairro;
-  document.getElementById('cidade').innerHTML = localidade;
-  document.getElementById('estado').innerHTML = estado;
+  document.getElementById('result-logradouro').innerHTML = logradouro;
+  document.getElementById('result-bairro').innerHTML = bairro;
+  document.getElementById('result-cidade').innerHTML = localidade;
+  document.getElementById('result-estado').innerHTML = estado;
 }
